4e24f761cbceb9468bc091705f2d1636f31dff79
wzhlyforever <814808756@qq.com>
2020-08-31 23:29:10 +0800

    based on spring-mvc to develop web application

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,9 +3,9 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   //表明这是spring boot应用
 public class TacoCloudApplication {
-
+    //运行此应用
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
